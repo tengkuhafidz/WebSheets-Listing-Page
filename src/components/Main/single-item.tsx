e17e5a8c1b6caf9cfd8b715ffb71284c808319c5
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-05-04 01:46:30 +0800

    update styling and make propertuies more adaptive

--- src/components/Main/single-item.tsx
+++ src/components/Main/single-item.tsx
@@ -10,19 +10,38 @@ const SingleItem: React.FC<Props> = ({ item }) => {
 
   const renderImage = () => {
     if (hasProperty(item.image)) {
-      console.log('item.image', item.image)
       return <img className="w-full rounded-t-lg" src={item.image} alt={`Image of ${item.title}`} />
     }
     return <></>
   }
+
+  const renderTitle = () => {
+    if (hasProperty(item.title)) {
+      return <div className="font-bold text-gray-100 text-xl mb-2">{item.title}</div>
+    }
+    return <></>
+  }
+
+  const renderDescription = () => {
+    if (hasProperty(item.description)) {
+      return <p className="text-gray-400 font-light text-base">{item.description}</p>
+    }
+    return <></>
+  }
   return (
-    <div className={`max-w-sm rounded-lg shadow-lg mt-8 ${hasProperty(item.url) && 'hover:shadow-2xl cursor-pointer'}`}>
-      {renderImage()}
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{item.title}</div>
-        <p className="text-gray-700 text-base">{item.description}</p>
+    <a href={item.url}>
+      <div
+        className={`max-w-sm rounded-lg shadow-lg mt-8 bg-indigo-800 ${
+          hasProperty(item.url) && 'hover:shadow-2xl cursor-pointer'
+        }`}
+      >
+        {renderImage()}
+        <div className="px-6 py-4">
+          {renderTitle()}
+          {renderDescription()}
+        </div>
       </div>
-    </div>
+    </a>
   )
 }
 
