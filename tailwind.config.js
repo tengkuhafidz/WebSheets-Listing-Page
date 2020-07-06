ac2d22f90b4848051f764417064b7955a90bd640
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-07-07 03:14:19 +0800

    add brand color on tailwind-css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
       boxShadow: {
         white: '0 20px 25px -5px rgba(251, 251, 251, 0.1), 0 10px 10px -5px rgba(251, 251, 251, 0.04)',
       },
+      colors: {
+        brand: '#049663',
+      },
     },
   },
   variants: {},
