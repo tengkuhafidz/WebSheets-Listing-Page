6f342d7923734dab724979c04f0f5091be74c1c7
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-09-19 18:19:05 +0800

    explicitly write content of seo

--- src/components/ListingPage/seo.tsx
+++ src/components/ListingPage/seo.tsx
@@ -43,7 +43,7 @@ const SEO: React.FC<Props> = ({ title, description, image, twitterCard }) => {
   return (
     <div>
       <Helmet title={metaTitle} titleTemplate={titleTemplate} defer={false}>
-        <meta name="description" content={metaDescription} />
+        {/* <meta name="description" content={metaDescription} />
         <meta name="image" content={metaImage} />
         <meta name="og:title" content={metaTitle} />
         <meta name="og:description" content={metaDescription} />
@@ -53,7 +53,22 @@ const SEO: React.FC<Props> = ({ title, description, image, twitterCard }) => {
         <meta name="twitter:creator" content={twitterUsername} />
         <meta name="twitter:title" content={metaTitle} />
         <meta name="twitter:description" content={metaDescription} />
-        <meta name="twitter:image" content={metaImage} />
+        <meta name="twitter:image" content={metaImage} /> */}
+        <title>Additive Halal Check</title>
+        <meta name="title" content="Additive Halal Check" />
+        <meta name="description" content="A list of ECodes/additives with their details and halal status" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ecodehalalcheck.com/" />
+        <meta property="og:title" content="Additive Halal Check" />
+        <meta property="og:description" content="A list of ECodes/additives with their details and halal status" />
+        <meta property="og:image" content="/app-banner.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://ecodehalalcheck.com/" />
+        <meta property="twitter:title" content="Additive Halal Check" />
+        <meta property="twitter:description" content="A list of ECodes/additives with their details and halal status" />
+        <meta property="twitter:image" content="/app-banner.png" />
       </Helmet>
     </div>
   )
